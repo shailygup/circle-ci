2cc19f544c7622f6aafeba9096a8706df1e862fa
Shaily Gupta <shaily.gupta@cloudreach.com>
2019-07-25 14:11:30 -0700

    One-more-test

--- scripts/converge.sh
+++ scripts/converge.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 declare -a organization_files
-# files_modified="$(git --no-pager diff --name-only master)"
-# CIRCLE_BRANCH='One-more-test'
-echo "$CIRCLE_BRANCH"
+CIRCLE_BRANCH='One-more-test'
 SAVEIFS=$IFS   # Save current IFS
 IFS=$'\n'      # Change IFS to new line
 IFS=$SAVEIFS   # Restore IFS
-
-
 
 converge_organization () {
     for file_name in $files_modified; do
@@ -24,7 +20,6 @@ converge_all () {
 
 if [[ ! -z "$CIRCLE_BRANCH" && "$CIRCLE_BRANCH" != "master" ]]; then
 	git checkout -q master && git reset  -q --soft origin/master && git checkout -q $CIRCLE_BRANCH && files_modified="$(git --no-pager diff --name-only master)"
-    echo ${files_modified[*]}
     if [[ ${files_modified[*]} =~ signalfx ]]; then
         if [[ ${files_modified[@]} =~ specs || ${files_modified[@]} =~ detectors ]]; then
             converge_all
@@ -33,26 +28,3 @@ if [[ ! -z "$CIRCLE_BRANCH" && "$CIRCLE_BRANCH" != "master" ]]; then
         fi
     fi
 fi
-
-
-
-# if [[ ($file_name = *"specs"*  || $file_name = *"detectors"* ) ]]; then
-        #     converge_all
-    # for file_name in $files_modified; do
-    #     if [[ $file_name = "signalfx"* ]]; then
-    #         if [[ ($file_name = *"specs"*  || $file_name = *"detectors"* ) ]]; then
-    #             converge_all
-    #         else 
-    #             second+=("$file_name")
-    #             converge_organization 
-    #         # echo "python cli.py --pd-token 901ff31c40ab4a02ba6a917f02760578 --debug converge --email \"support@cloudreach.com\" --password \"nxICCc%4mJq#9v16\" --dry-run $file_name" 
-    #         fi
-
-    #     else
-    #         continue 
-    #     fi
-    # done
-     
-    # converge_organization $second
-    # echo "${second[@]}"
-
