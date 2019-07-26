eff86c46cbca340df23d16a699c3e3298e8b70db
Shaily Gupta <shaily.gupta@cloudreach.com>
2019-07-26 08:37:42 -0700

    Something-else

--- scripts/converge.sh
+++ scripts/converge.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 declare -a organization_files
-# files_modified="$(git --no-pager diff --name-only master)"
+echo '$CIRCLE_SHA1'
+echo '$CIRCLE_BRANCH'
 # CIRCLE_BRANCH='One-more-test'
-echo "$CIRCLE_BRANCH"
-SAVEIFS=$IFS   # Save current IFS
 IFS=$'\n'      # Change IFS to new line
-IFS=$SAVEIFS   # Restore IFS
 
-
-
-converge_organization () {
+converge_organization_dry_run () {
     for file_name in $files_modified; do
         if [[ $file_name = *"organization"* ]]; then
             organization_files+=("$file_name")
@@ -18,41 +14,23 @@ converge_organization () {
     echo "python cli.py --pd-token 901ff31c40ab4a02ba6a917f02760578 --debug converge --email \"support@cloudreach.com\" --password \"nxICCc%4mJq#9v16\" --dry-run ${organization_files[@]}" 
 }
 
-converge_all () {
+converge_all_dry_run () {
     echo "python cli.py --pd-token 901ff31c40ab4a02ba6a917f02760578 --debug converge --email \"support@cloudreach.com\" --password \"nxICCc%4mJq#9v16\" --dry-run" 
+}
+
+check_signalfx_directories(){
+    if [[ ${files_modified[*]} =~ signalfx ]]; then
+        if [[ ${files_modified[@]} =~ specs || ${files_modified[@]} =~ detectors ]]; then
+            converge_all_dry_run
+        elif [[ ${files_modified[@]} =~ organizations ]]; then
+            converge_organization_dry_run
+        fi
+    fi
 }
 
 if [[ ! -z "$CIRCLE_BRANCH" && "$CIRCLE_BRANCH" != "master" ]]; then
 	git checkout -q master && git reset  -q --soft origin/master && git checkout -q $CIRCLE_BRANCH && files_modified="$(git --no-pager diff --name-only master)"
-    echo ${files_modified[*]}
-    if [[ ${files_modified[*]} =~ signalfx ]]; then
-        if [[ ${files_modified[@]} =~ specs || ${files_modified[@]} =~ detectors ]]; then
-            converge_all
-        elif [[ ${files_modified[@]} =~ organizations ]]; then
-            converge_organization
-        fi
-    fi
+    check_signalfx_directories
+elif [[ "$CIRCLE_BRANCH" == "master" ]]; then
+    check_signalfx_directories
 fi
-
-
-
-# if [[ ($file_name = *"specs"*  || $file_name = *"detectors"* ) ]]; then
-        #     converge_all
-    # for file_name in $files_modified; do
-    #     if [[ $file_name = "signalfx"* ]]; then
-    #         if [[ ($file_name = *"specs"*  || $file_name = *"detectors"* ) ]]; then
-    #             converge_all
-    #         else 
-    #             second+=("$file_name")
-    #             converge_organization 
-    #         # echo "python cli.py --pd-token 901ff31c40ab4a02ba6a917f02760578 --debug converge --email \"support@cloudreach.com\" --password \"nxICCc%4mJq#9v16\" --dry-run $file_name" 
-    #         fi
-
-    #     else
-    #         continue 
-    #     fi
-    # done
-     
-    # converge_organization $second
-    # echo "${second[@]}"
-
