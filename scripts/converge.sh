740a652cd962c09561123441b966235daedbf3ef
Shaily Gupta <shaily.gupta@cloudreach.com>
2019-07-26 08:15:34 -0700

    One-more-test

--- scripts/converge.sh
+++ scripts/converge.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 declare -a organization_files
 CIRCLE_BRANCH='One-more-test'
-SAVEIFS=$IFS   # Save current IFS
 IFS=$'\n'      # Change IFS to new line
 
 converge_organization () {
@@ -17,8 +16,7 @@ converge_all () {
     echo "python cli.py --pd-token 901ff31c40ab4a02ba6a917f02760578 --debug converge --email \"support@cloudreach.com\" --password \"nxICCc%4mJq#9v16\" --dry-run" 
 }
 
-if [[ ! -z "$CIRCLE_BRANCH" && "$CIRCLE_BRANCH" != "master" ]]; then
-	git checkout -q master && git reset  -q --soft origin/master && git checkout -q $CIRCLE_BRANCH && files_modified="$(git --no-pager diff --name-only master)"
+check_signalfx_directories(){
     if [[ ${files_modified[*]} =~ signalfx ]]; then
         if [[ ${files_modified[@]} =~ specs || ${files_modified[@]} =~ detectors ]]; then
             converge_all
@@ -26,4 +24,16 @@ if [[ ! -z "$CIRCLE_BRANCH" && "$CIRCLE_BRANCH" != "master" ]]; then
             converge_organization
         fi
     fi
+}
+
+if [[ ! -z "$CIRCLE_BRANCH" && "$CIRCLE_BRANCH" != "master" ]]; then
+	git checkout -q master && git reset  -q --soft origin/master && git checkout -q $CIRCLE_BRANCH && files_modified="$(git --no-pager diff --name-only master)"
+    # if [[ ${files_modified[*]} =~ signalfx ]]; then
+    #     if [[ ${files_modified[@]} =~ specs || ${files_modified[@]} =~ detectors ]]; then
+    #         converge_all
+    #     elif [[ ${files_modified[@]} =~ organizations ]]; then
+    #         converge_organization
+    #     fi
+    # fi
+    check_signalfx_directories
 fi
